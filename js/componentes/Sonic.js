0efa668dd00ee0c7dbbbcfd2ae085da4f49f27c2
faustinopsy <rodrigohipnose@gmail.com>
2025-04-25 19:26:08 -0300

    o sprite sonic é ruim preciso escolher outro

--- js/componentes/Sonic.js
+++ js/componentes/Sonic.js
@@ -1,20 +1,55 @@
-export default class Sonic{
-    constructor(){
+export default class Sonic {
+    constructor(canvas) {
+        this.canvas = canvas;
+        this.ctx = canvas.getContext("2d");
 
-    }
-    andar(){
+        this.sprite = new Image();
+        this.sprite.src = "../img/sonic.png";
+        this.sprite.onload = () => this.desenhar();
 
+        this.larguraFrame = 40;
+        this.alturaFrame = 50;
+        this.totalFrames = 6;
+        this.frameAtual = 0;
+        this.linhaAtual = 1;
+        this.posicaoX = 100;
+        this.posicaoY = 200;
     }
-    correr(){
 
+    desenhar() {
+        const espaço = 0.5;
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        const sx = Math.floor(this.frameAtual) * (this.larguraFrame + espaço);
+        const sy = Math.floor(this.linhaAtual) * this.alturaFrame;
+        this.ctx.drawImage(
+            this.sprite,
+            sx, sy, this.larguraFrame, this.alturaFrame,
+            this.posicaoX, this.posicaoY, this.larguraFrame, this.alturaFrame
+        );
     }
-    parar(){
 
+    andar(direcao) {
+        if (direcao === "direita") {
+            this.linhaAtual = 1;
+            this.frameAtual = (this.frameAtual + 1) % this.totalFrames;
+           // this.posicaoX += 10;
+        } else if (direcao === "esquerda") {
+            this.linhaAtual = 1;
+            this.frameAtual = (this.frameAtual + 1) % this.totalFrames;
+            //this.posicaoX -= 10;
+        }
+        this.desenhar();
     }
-    pular(){
 
-    }
-    transformar(){
+    parar() {
         
+    }
+
+    pular() {
+        console.log("Pulo!");
+    }
+
+    transformar() {
+        console.log("Transformação!");
     }
 }
--- js/main.js
+++ js/main.js
@@ -1,25 +1,16 @@
-const canvas = document.querySelector("#canva")
-const ctx = canvas.getContext('2d')
-const imagem = new Image();
-let x = 70
-imagem.src = '../img/sonic.png';
-imagem.onload = function() {
-   desenharPersonagem(x)
-   
-}
+import Sonic from './componentes/Sonic.js';
 
-function desenharPersonagem(pos){
-    ctx.drawImage(imagem, pos, 45, 40, 50 , 30, 200, 50, 50);
-   
-}
+const canvas = document.querySelector("#canva");
+const sonic = new Sonic(canvas);
 
-document.addEventListener("keydown",(e)=>{
-    if (e.keyCode == 37) {
-            x -= 30;
-            desenharPersonagem(x);
-        }
-        else if (e.keyCode == 39) {
-            x += 30;
-            desenharPersonagem(x);
-        }
-   })
+document.addEventListener("keydown", (e) => {
+    if (e.key === "ArrowLeft") {
+        sonic.andar("esquerda");
+    } else if (e.key === "ArrowRight") {
+        sonic.andar("direita");
+    } else if (e.key === "ArrowUp") {
+        sonic.pular();
+    } else if (e.key === " ") {
+        sonic.transformar();
+    }
+});
