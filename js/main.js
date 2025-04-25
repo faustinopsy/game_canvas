912f60ae19c175600f718b6fb1e14767b5d97fc2
faustinopsy <rodrigohipnose@gmail.com>
2025-04-25 17:21:02 -0300

    teste final sobre posições posições

--- js/main.js
+++ js/main.js
@@ -2,9 +2,17 @@ const canvas = document.querySelector("#canva")
 const ctx = canvas.getContext('2d')
 ctx.beginPath();
 ctx.arc(100, 50, 40, 0,  Math.PI);
+
+ctx.lineWidth = 5;
+ctx.strokeStyle = 'red';
 ctx.moveTo(100, 50);
 ctx.lineTo(100, 200);
 ctx.stroke();
 
-ctx.fillRect(110, 150, 50, 100)
-ctx.fillRect(85, 180, 100, 20)
+ctx.lineWidth = 2;
+ctx.strokeStyle = 'blue';
+ctx.strokeRect(110, 150, 50, 100)
+ctx.lineWidth = 2;
+
+ctx.fillStyle = 'red';
+ctx.fillRect(85, 180, 100, 20);
