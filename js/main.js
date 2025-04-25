79c719cffa3e8cd2c04ced1a08b4941e5743b6b5
faustinopsy <rodrigohipnose@gmail.com>
2025-04-25 18:27:51 -0300

    capturar telcas para movimentar a posição

--- js/main.js
+++ js/main.js
@@ -1,15 +1,25 @@
 const canvas = document.querySelector("#canva")
 const ctx = canvas.getContext('2d')
 const imagem = new Image();
+let x = 70
 imagem.src = '../img/sonic.png';
 imagem.onload = function() {
-   let x = 70
-   const loop = setInterval(() => {
-    x += 30
-    if(x > 400){
-        clearInterval(loop)
-    }
-    ctx.drawImage(imagem, x, 45, 40, 50 , 30, 200, 50, 50);
-   }, 200);
+   desenharPersonagem(x)
    
 }
+
+function desenharPersonagem(pos){
+    ctx.drawImage(imagem, pos, 45, 40, 50 , 30, 200, 50, 50);
+   
+}
+
+document.addEventListener("keydown",(e)=>{
+    if (e.keyCode == 37) {
+            x -= 30;
+            desenharPersonagem(x);
+        }
+        else if (e.keyCode == 39) {
+            x += 30;
+            desenharPersonagem(x);
+        }
+   })
