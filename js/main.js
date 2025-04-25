587c089d4f08594e740418d6827c23f19da1ba7f
faustinopsy <rodrigohipnose@gmail.com>
2025-04-25 18:16:43 -0300

    movimento automatico

--- js/main.js
+++ js/main.js
@@ -3,7 +3,13 @@ const ctx = canvas.getContext('2d')
 const imagem = new Image();
 imagem.src = '../img/sonic.png';
 imagem.onload = function() {
-   let x = 30
-   ctx.drawImage(imagem, x, 45, 40, 50 , 30, 200, 50, 50);
+   let x = 70
+   const loop = setInterval(() => {
+    x += 30
+    if(x > 400){
+        clearInterval(loop)
+    }
+    ctx.drawImage(imagem, x, 45, 40, 50 , 30, 200, 50, 50);
+   }, 200);
    
 }
